3a0c97729a027aa4b63e17cafc375244dbd94b89
Francisco Arias <fco.arias10@gmail.com>
2020-05-18 09:06:33 -0400

    base of the project
    activities, fragments and dialog fragments creation

---
+++ app/src/main/java/com/fran/platziconf/ui/activities/SplashscreenActivity.kt
@@ -0,0 +1,13 @@
+package com.fran.platziconf.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.fran.platziconf.R
+
+class SplashscreenActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splashscreen)
+    }
+}
